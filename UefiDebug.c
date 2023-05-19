3c217d38469356d4a2255204c44b6a33a83956c0
Raul Mario <64203039+raulmrio28-git@users.noreply.github.com>
2023-05-19 18:14:44 +0300

    other types of uefidebug asserts added
    -EFI_SUCCESS assert checks
    -moved the assert print when expression failed

--- UefiDebug.c
+++ UefiDebug.c
@@ -71,7 +71,6 @@ DebugPrtPrint(
 	IN INTN nLine
 )
 {
-	UINTN   nReturn;
 	CHAR8   pszBuffer[DBGMSG_MAX_CHARS];
 	if (pszAssertType == NULL || pszExpression == NULL || pszFile == NULL)
 		return ASSERTION_NEVER_GET_HERE;
@@ -81,6 +80,83 @@ DebugPrtPrint(
 		AsciiSPrint(pszBuffer, sizeof(pszBuffer), "%a:%a->Function not implemented! (file %a, line %d)\n", pszAssertType, pszFunction, pszFile, nLine);
 	else
 		AsciiSPrint(pszBuffer, sizeof(pszBuffer), "%a:%a->%a (file %a, line %d)\n", pszAssertType, pszFunction, pszExpression, pszFile, nLine);
+	SerialPortWrite((UINT8 *)pszBuffer, AsciiStrLen(pszBuffer));
+	return nAssertLevel;
+}
+
+/*
+** ===========================================================================
+** Function: DebugPrtPrintMsg()
+** Description: Writes n characters of input buffer to output buffer, whist u-
+** sing a custom message shown for debugging...
+** Input:
+**		pszInChar: input buffer
+**		nChars: characters to write to output
+** Output: function itself
+** Return value: NULL -> Failure, buffer -> Success
+** ===========================================================================
+*/
+EFI_STATUS
+DebugPrtPrintMsg(
+	IN INTN nAssertLevel,
+	IN CHAR8 *pszAssertType,
+	IN CHAR8 *pszFunction,
+	IN CHAR8 *pszExpression,
+	IN CHAR8 *pszFile,
+	IN INTN nLine,
+	IN CHAR8 *pszMessage,
+	...
+)
+{
+	CHAR8   pszBufferMsg[DBGMSG_MAX_CHARS];
+	CHAR8   pszBuffer[DBGMSG_MAX_CHARS*2];
+	if (pszAssertType == NULL || pszExpression == NULL || pszFile == NULL)
+		return ASSERTION_NEVER_GET_HERE;
+	VA_LIST pszArgList;
+	VA_START(pszArgList, pszMessage);
+	AsciiVSPrint(pszBufferMsg, sizeof(pszBufferMsg), pszMessage, pszArgList);
+	VA_END(pszArgList);
+	if (nAssertLevel == ASSERTION_NEVER_GET_HERE)
+		AsciiSPrint(pszBuffer, sizeof(pszBuffer), "%a:%a->Cannot continue... (file %a, line %d)\n%a\n", pszAssertType, pszFunction, pszFile, nLine, pszBufferMsg);
+	if (nAssertLevel == ASSERTION_NOT_IMPLEMENTED_YET)
+		AsciiSPrint(pszBuffer, sizeof(pszBuffer), "%a:%a->Function not implemented! (file %a, line %d)\n%a\n", pszAssertType, pszFunction, pszFile, nLine, pszBufferMsg);
+	else
+		AsciiSPrint(pszBuffer, sizeof(pszBuffer), "%a:%a->%a (file %a, line %d)\n%a\n", pszAssertType, pszFunction, pszExpression, pszFile, nLine, pszBufferMsg);
+	SerialPortWrite((UINT8 *)pszBuffer, AsciiStrLen(pszBuffer));
+	return nAssertLevel;
+}
+
+/*
+** ===========================================================================
+** Function: DebugPrtPrintMsgOnly()
+** Description: Same as above but no expression string
+** Input:
+**		pszInChar: input buffer
+**		nChars: characters to write to output
+** Output: function itself
+** Return value: NULL -> Failure, buffer -> Success
+** ===========================================================================
+*/
+EFI_STATUS
+DebugPrtPrintMsgOnly(
+	IN INTN nAssertLevel,
+	IN CHAR8 *pszAssertType,
+	IN CHAR8 *pszFunction,
+	IN CHAR8 *pszFile,
+	IN INTN nLine,
+	IN CHAR8 *pszMessage,
+	...
+)
+{
+	CHAR8   pszBufferMsg[DBGMSG_MAX_CHARS];
+	CHAR8   pszBuffer[DBGMSG_MAX_CHARS * 2];
+	if (pszAssertType == NULL || pszFile == NULL)
+		return ASSERTION_NEVER_GET_HERE;
+	VA_LIST pszArgList;
+	VA_START(pszArgList, pszMessage);
+	AsciiVSPrint(pszBufferMsg, sizeof(pszBufferMsg), pszMessage, pszArgList);
+	VA_END(pszArgList);
+	AsciiSPrint(pszBuffer, sizeof(pszBuffer), "%a:%a->%a (file %a, line %d)\n", pszAssertType, pszFunction, pszBufferMsg, pszFile, nLine);
 	SerialPortWrite((UINT8 *)pszBuffer, AsciiStrLen(pszBuffer));
 	return nAssertLevel;
 }
--- UefiDebug.h
+++ UefiDebug.h
@@ -1,4 +1,4 @@
-/* 
+/*
 ** ===========================================================================
 ** File: UefiDebug.h
 ** Description: UEFI debug addon
@@ -73,37 +73,79 @@ DebugPrtPrint(
 );
 
 /*
+** ===========================================================================
+** Function: DebugPrtPrintMsg()
+** Description: Writes n characters of input buffer to output buffer, whist u-
+** sing a custom message shown for debugging...
+** Input:
+**		pszInChar: input buffer
+**		nChars: characters to write to output
+** Output: function itself
+** Return value: NULL -> Failure, buffer -> Success
+** ===========================================================================
+*/
+EFI_STATUS
+DebugPrtPrintMsg(
+	IN INTN nAssertLevel,
+	IN CHAR8 *pszAssertType,
+	IN CHAR8 *pszFunction,
+	IN CHAR8 *pszExpression,
+	IN CHAR8 *pszFile,
+	IN INTN nLine,
+	IN CHAR8 *pszMessage,
+	...
+);
+
+/*
+** ===========================================================================
+** Function: DebugPrtPrintMsgOnly()
+** Description: Same as above but no expression string
+** Input:
+**		pszInChar: input buffer
+**		nChars: characters to write to output
+** Output: function itself
+** Return value: NULL -> Failure, buffer -> Success
+** ===========================================================================
+*/
+EFI_STATUS
+DebugPrtPrintMsgOnly(
+	IN INTN nAssertLevel,
+	IN CHAR8 *pszAssertType,
+	IN CHAR8 *pszFunction,
+	IN CHAR8 *pszFile,
+	IN INTN nLine,
+	IN CHAR8 *pszMessage,
+	...
+);
+
+/*
 **----------------------------------------------------------------------------
 **  Definitions (macros for assertion)
 **----------------------------------------------------------------------------
 */
 
 #define ASSERT_DEBUG(type, expression) DebugPrtPrint(type, #type, __func__, #expression, __FILE__, __LINE__)
+// leaving it here unless we can figure out how to parse this...
+#define ASSERT_DEBUG_MSGONLY(msg, ...) DebugPrtPrintMsgOnly(ASSERTION_ASSERT, "ASSERTION_MSG", __func__, __FILE__, __LINE__, msg, ##__VA_ARGS__)
+#define ASSERT_DEBUG_MSG(type, expression, msg, ...) DebugPrtPrintMsg(__func__, #expression, __FILE__, __LINE__, msg, ##__VA_ARGS__)
 
 #define ASSERT_ASSERT(expr)    \
-	ASSERT_DEBUG(ASSERTION_ASSERT, expr);\
-	if (!(expr)) { return EFI_LOAD_ERROR; }
+	if (!(expr)) {ASSERT_DEBUG(ASSERTION_ASSERT, expr); return EFI_LOAD_ERROR; }
 #define ASSERT_REQUIRE(expr)    \
-	ASSERT_DEBUG(ASSERTION_REQUIRE, expr);\
-	if (!(expr)) { return EFI_LOAD_ERROR; }
+	if (!(expr)) {ASSERT_DEBUG(ASSERTION_ASSERT, expr); return EFI_LOAD_ERROR; }
 #define ASSERT_ENSURE(expr)    \
 	ASSERT_DEBUG(ASSERTION_ENSURE, expr);\
-	if (!(expr)) { return EFI_LOAD_ERROR; }
+	if (!(expr)) {ASSERT_DEBUG(ASSERTION_ASSERT, expr); return EFI_LOAD_ERROR; }
 #define ASSERT_CHECK(expr)        \
-	ASSERT_DEBUG(ASSERTION_CHECK, expr);\
-	if (!(expr)) { return EFI_LOAD_ERROR; }
+	if (!(expr)) {ASSERT_DEBUG(ASSERTION_ASSERT, expr); return EFI_LOAD_ERROR; }
 #define ASSERT_ASSERT_EFISTATUS(expr)    \
-	ASSERT_DEBUG(ASSERTION_ASSERT, expr);\
-	if ((expr) != EFI_SUCCESS) { return EFI_LOAD_ERROR; }
+	if ((expr) != EFI_SUCCESS) {ASSERT_DEBUG(ASSERTION_ASSERT, expr); return EFI_LOAD_ERROR; }
 #define ASSERT_REQUIRE_EFISTATUS(expr)    \
-	ASSERT_DEBUG(ASSERTION_REQUIRE, expr);\
-	if ((expr) != EFI_SUCCESS) { return EFI_LOAD_ERROR; }
+	if ((expr) != EFI_SUCCESS) {ASSERT_DEBUG(ASSERTION_ASSERT, expr); return EFI_LOAD_ERROR; }
 #define ASSERT_ENSURE_EFISTATUS(expr)    \
-	ASSERT_DEBUG(ASSERTION_ENSURE, expr);\
-	if ((expr) != EFI_SUCCESS) { return EFI_LOAD_ERROR; }
+	if ((expr) != EFI_SUCCESS) {ASSERT_DEBUG(ASSERTION_ASSERT, expr); return EFI_LOAD_ERROR; }
 #define ASSERT_CHECK_EFISTATUS(expr)        \
-	ASSERT_DEBUG(ASSERTION_CHECK, expr);\
-	if ((expr) != EFI_SUCCESS) { return EFI_LOAD_ERROR; }
+	if ((expr) != EFI_SUCCESS) {ASSERT_DEBUG(ASSERTION_ASSERT, expr); return EFI_LOAD_ERROR; }
 #define ASSERT_NEVER_GET_HERE()    ASSERT_DEBUG(ASSERTION_NEVER_GET_HERE, NULL);
 #define ASSERT_NOT_IMPLEMENTED_YET()    ASSERT_DEBUG(ASSERTION_NOT_IMPLEMENTED_YET, NULL);
 
